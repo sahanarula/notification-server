08ce5fe58bea37ea5e23b852e330b499f8c0a3d1
sahil narula <sahiln123@gmail.com>
2016-05-05 12:34:40 +0530

    added tracker controller

---
+++ api/controllers/TrackerController.js
@@ -0,0 +1,11 @@
+/**
+ * TrackerController
+ *
+ * @description :: Server-side logic for managing trackers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
